86e84693a1a48c3f65e507d2b68f94254ca2aef0
Vitaliy Artemov <olivera507224@yandex.ru>
2024-01-27 21:23:00 +0000

    box (event_watchers): add files with todos

---
+++ samples/box/event_watchers/system_events.ts
@@ -0,0 +1,3 @@
+// https://www.tarantool.io/en/doc/latest/reference/reference_lua/box_events/system_events/
+
+/** @todo implement loa, net.box packages */
---
+++ samples/box/event_watchers/watch.ts
@@ -0,0 +1,3 @@
+// https://www.tarantool.io/en/doc/latest/reference/reference_lua/box_events/watch/
+
+/** @todo implement log package */
